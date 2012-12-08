70234228478a18cfea1d2b1ef43042e5886a2e34
Onur Ferhat <tiendan@gmail.com>
2012-12-08 19:31:28 +0100

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skeptical::Application.config.secret_token = '72a86572e1a125c8e133a8c7564d501ffe7870c3901d07f75c3036afb97b04c09ab3d34af5e9d97789183928efb959b587583b4ca010c5afbd493a08a29ee9fe'
